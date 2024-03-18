a5b349bea60e6b2a9818a5dca20f1b5129fddd5c
Vince Jerald Villamora <1276544+vincejv@users.noreply.github.com>
2024-03-19 07:58:11 +0800

    Adapt entrypoint to alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,8 @@ if [ "${1:0:1}" = '-' ]; then
     set -- fnp-bot "$@"
 fi
 
-if [ $EUID -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
     exec "$@"
 else
-    # honor groups supplied via 'docker run --group-add ...' but drop 'root'
-    # (also removes 'fnp-bot' since we unconditionally add it and don't want it listed twice)
-    groups="fnp-bot"
-    extra_groups="$(id -Gn || true)"
-    for group in $extra_groups; do
-        case "$group" in
-            root | fnp-bot) ;;
-            *) groups="$groups,$group" ;;
-        esac
-    done
-    exec setpriv --reuid fnp-bot --regid fnp-bot --groups "$groups" "$@"
+    exec su-exec fnp-bot "$@"
 fi
