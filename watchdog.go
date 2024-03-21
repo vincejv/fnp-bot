1bda2227c16f7e65618760260616213f093236b5
Vince Jerald Villamora <1276544+vincejv@users.noreply.github.com>
2024-03-21 11:04:52 +0800

    Fix wsWatchdog logic

--- watchdog.go
+++ watchdog.go
@@ -7,10 +7,10 @@ import (
 )
 
 var wsWatchdogTimerInitVal = 35 // 35 seconds
-var wsWatchdogTimer = wsWatchdogTimerInitVal
 
 func wsWatchdog(ctx context.Context, roomId string) {
 	log.Println("Starting WS Watchdog")
+	wsWatchdogTimer := wsWatchdogTimerInitVal
 	for wsWatchdogTimer > 0 {
 		// poll and check every 1 sec if ws is already established
 		time.Sleep(1 * time.Second)
@@ -18,10 +18,9 @@ func wsWatchdog(ctx context.Context, roomId string) {
 		if pingPongWatchdog.Get() >= 1 {
 			// ws Got Ponged
 			pingPongWatchdog.Reset()
-			wsWatchdogTimer = wsWatchdogTimerInitVal
-			return
+			wsWatchdogTimer = wsWatchdogTimerInitVal // trickle to initVal
 		}
 	}
 	go reloadChatPage(ctx, roomId, "WS PingPong failed, reloading page")
-	wsWatchdog(ctx, roomId)
+	wsWatchdog(ctx, roomId) //lint:ignore SA5007 Expected to recurse infinitely as this is a watchdog timer, thread sleeps are implemented
 }
